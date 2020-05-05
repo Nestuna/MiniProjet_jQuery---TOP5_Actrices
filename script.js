418940f3a563ae94b68f33735589c2353f13ceb2
Nestuna <nicolasantunespereira@gmail.com>
2020-05-05 12:12:01 +0200

    HTML + CSS FINI

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+    var $mainMenuItems = $('#main-menu ul').children('li'),
+        totalMainMenuItems = $mainMenuItems.length,
+        openedIndex =  -1,
+
+        init = function() {
+
+            $mainMenuItems.children('.images').click(function(){
+                
+                var newIndex = $(this).parent().index();
+                $item = mainMenuItems.eq(newIndex);
+                
+            });
+        };
 
 });

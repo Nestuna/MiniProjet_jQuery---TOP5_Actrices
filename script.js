b08f8b9f2da680e06354ff43bd4fb24a3763bdfc
Nestuna <nicolasantunespereira@gmail.com>
2020-05-06 00:02:17 +0200

    animation

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
 $(document).ready(function(){
-    var $mainMenuItems = $('#main-menu ul').children('li'),
+    console.log("Page chargée.");
+
+    // VARIABLES GLOBALES
+    var $mainMenuItems = $('#main-menu ul').children('li');
         totalMainMenuItems = $mainMenuItems.length,
-        openedIndex =  -1,
+        openedIndex =  -1;
 
-        init = function() {
+    // FONCTIONS    
+    function init() {
 
-            $mainMenuItems.children('.images').click(function(){
-                
-                var newIndex = $(this).parent().index();
-                $item = mainMenuItems.eq(newIndex);
-                
-            });
-        };
+        $mainMenuItems.children('.images').click(function(){
+            console.log("Image cliquée");
+
+            var newIndex = $(this).parent().index(),
+                $item = $mainMenuItems.eq(newIndex);
+
+            if (openedIndex === newIndex) {
+                animateItem($item, false, 250)
+                openedIndex = -1;
+            }
+            else {
+                if (validIndex(newIndex)) {
+                    var $item_toClose = $mainMenuItems.eq(openedIndex);
+                    animateItem($item_toClose, false, 250);
+                    
+                    animateItem($item, true, 250);
+                    openedIndex = newIndex;        
+                }
+            }
+        });
+    };
+
+    function validIndex(indexToCheck) {
+
+        return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
+    }
+    
+    function animateItem($item, toOpen, speed) {
+
+        var $colorImage = $item.find('.color'),
+            itemParam = toOpen ? {width:'420px'} : {width:'140px'}, 
+            colorImageParam = toOpen ? {left:0} : {left:'140px'};
+
+        $colorImage.animate(colorImageParam, speed);
+        $item.animate(itemParam, speed);
+    }
+
+    // PROGRAMME
+    init();
 
 });

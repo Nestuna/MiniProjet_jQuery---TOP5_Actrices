679ca529068ecc22548644703efeab28e899abd4
Nestuna <nicolasantunespereira@gmail.com>
2020-05-11 22:37:34 +0200

    boutons

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ $(document).ready(function(){
     console.log("Page chargée.");
 
     // VARIABLES GLOBALES
-    var $mainMenuItems = $('#main-menu ul').children('li');
+    var $mainMenuItems = $('#main-menu ul').children('li'),
         totalMainMenuItems = $mainMenuItems.length,
-        openedIndex =  -1;
+        openedIndex =  0;
 
     // FONCTIONS    
     function init() {
+    // Fonction d'initialisation du programme
+
+    bindEvents(); // On charge les évenements d'animation
+
+    if (validIndex(openedIndex)){
+        // Animation de l'item à l'arrivée dans la page
+            var $item_Init = $mainMenuItems.eq(openedIndex);
+            animateItem($item_Init, 750);
+        }
+    }
+
+    function bindEvents() {
+    // Fonction principale d'évenements d'animations
 
         $mainMenuItems.children('.images').click(function(){
             console.log("Image cliquée");
@@ -16,27 +29,33 @@ $(document).ready(function(){
                 $item = $mainMenuItems.eq(newIndex);
 
             if (openedIndex === newIndex) {
-                animateItem($item, false, 250)
+                animateItem($item, false, 250) // On ferme l'item selectionné
                 openedIndex = -1;
             }
             else {
-                if (validIndex(newIndex)) {
+                if (validIndex(newIndex)) { // Si un item est ouvert (sinon -1)
                     var $item_toClose = $mainMenuItems.eq(openedIndex);
-                    animateItem($item_toClose, false, 250);
+                    animateItem($item_toClose, false, 250); // On ferme l'item ouvert
                     
-                    animateItem($item, true, 250);
+                    animateItem($item, true, 250); // On ouvre l'item selectionné
                     openedIndex = newIndex;        
                 }
             }
         });
-    };
+    }
+    function checkAndAnimate(index) {
 
+    } 
+    
     function validIndex(indexToCheck) {
+    // Verifie si l'index fait partie de la liste
+    // Permet de verifier si un item est ouvert, tout en securisant le code
 
         return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
     }
     
     function animateItem($item, toOpen, speed) {
+    // Fonction d'ouverture/fermeture d'un item
 
         var $colorImage = $item.find('.color'),
             itemParam = toOpen ? {width:'420px'} : {width:'140px'}, 
